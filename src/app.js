2473a66b50ec75e9a2b6e9b677d113223bac5b2d
mdjoyanes-lgtm <mdjoyanes@gmail.com>
2026-04-08 09:26:15 +0000

    ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  let html = "";
+
+  for (let i = 0; i < 8; i++) {
+
+    let p = pronoun[Math.floor(Math.random() * pronoun.length)];
+    let a = adj[Math.floor(Math.random() * adj.length)];
+    let n = noun[Math.floor(Math.random() * noun.length)];
+
+    html += p + a + n + ".com <br>";
+  }
+
+  document.getElementById("domains").innerHTML = html;
+
 };
